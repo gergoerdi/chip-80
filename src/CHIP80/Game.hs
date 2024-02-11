27e7d4e4c2915da924ed6f8757a28b77dd9fed27
Gergo ERDI <gergo@erdi.hu>
2024-02-11 13:58:46 +0800

    For each game, store a set of keys that should map to joystick directions

--- src/CHIP80/Game.hs
+++ src/CHIP80/Game.hs
@@ -5,6 +5,7 @@ import CHIP80.Quirks
 import Z80
 import Z80.ZX0.Compress
 import Control.Monad
+import Data.Word
 import qualified Data.ByteString as BS
 import Data.String (fromString)
 
@@ -15,9 +16,11 @@ import Data.Aeson.Key (Key, toString)
 import Data.Text (unpack)
 import System.FilePath
 import Text.Printf
+import Numeric (readHex)
 
 data Game = Game
     { gameTitle :: String
+    , gameJoy :: (Word8, Word8, Word8, Word8, Word8) -- Up, down, left, right, fire
     , gameQuirks :: Quirks Bool
     , gameImage :: BS.ByteString
     }
@@ -35,6 +38,13 @@ readGames selected yamlPath = do
             gameQuirks = applyDefaults $ case fromJSON (Object vals) of
                 Error err -> error err
                 Success x -> x
+            gameJoy = case stringValue <$> KeyMap.lookup (fromString "joy") vals of
+                Just [u, d, l, r, f] -> (readJoy u, readJoy d, readJoy l, readJoy r, readJoy f)
+                  where
+                    readJoy c = case readHex [c] of
+                        [(x, "")] -> x
+                        _ -> error $ printf "readJoy: '%c'" c
+                _ -> (0x0, 0x0, 0x0, 0x0, 0x0)
         gameImage <- compressForward =<< BS.readFile fileName
         pure Game{..}
 
