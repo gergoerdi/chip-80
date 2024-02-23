a8b954a9a71d89e44ada3eae48de75e58f857af1
Gergo ERDI <gergo@erdi.hu>
2024-02-23 22:51:37 +0800

    GameBoy: Add `HLi` (`HL` with post-increment) opcodes

--- src/Target/GameBoy/Operations.hs
+++ src/Target/GameBoy/Operations.hs
@@ -20,3 +20,11 @@ ldhVia :: (LoadH tgt tmp, Load tmp src) => tmp -> tgt -> src -> Z80ASM
 ldhVia tmp tgt src = do
     ld tmp src
     ldh tgt tmp
+
+data HLi = HLi
+
+instance Load A [HLi] where
+    ld A [HLi] = code [0x2a]
+
+instance Load [HLi] A where
+    ld [HLi] A = code [0x22]
